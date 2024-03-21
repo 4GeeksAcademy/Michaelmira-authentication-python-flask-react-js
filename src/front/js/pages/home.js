c2a3ce037cc8e4755ea6149311f9c68f31c5ad70
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-21 20:46:57 +0000

    add button to handle login, make handle login a async function to also navigate to /profile

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,11 +1,21 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import { Navigate } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
+	const handleLogin = async(event) => {
+		// validate there is an email
+		// validate there is a password
+		const success = await actions.logIn({
+			email: email,
+			password: password
+		});
+		if (success) Navigate("/profile")
+	}
 
 	return (
 		<div>
@@ -20,6 +30,8 @@ export const Home = () => {
 					placeholder="password"
 					value={password}
 					onChange={(event) => setPassword(event.target.value)}/>
+				<button type="button" onClick={handleLogin}> submit
+				</button>
 				
 
 			</form>
