9f4bf8805fe4816db242ba817094823f041646c6
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-03-22 03:07:01 +0000

    create profile.js and create use context for store, and use effect and add imports

---
+++ src/front/js/pages/profile.js
@@ -0,0 +1,17 @@
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Profile = (props) => {
+    const {store, actions} = useContext(Context);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!store.token) {
+            navigate("/log-in");
+        }
+    }, [store.token, navigate]);
+    
+    return (
+        <h2> Hello Profile </h2>
+    );
+};
